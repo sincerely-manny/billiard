c7e7ef63f26a811f4156a141312a8ad295a04504
Kirill <sincerely.manny@gmail.com>
2024-03-25 16:51:42 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -118,7 +118,7 @@ const App = () => {
 
     return (
         <>
-            <p className="caption">Drag a ball to start fun. Click a ball to change it's color.</p>
+            <p className="caption">Drag a ball to start fun. Click a ball to change its color.</p>
             <canvas
                 ref={canvasRef}
                 width={1100}
